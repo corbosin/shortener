2d73004909511acd559ddf26f63a16d944ef3b87
corbosin <57488778+corbosin@users.noreply.github.com>
2021-01-19 12:33:01 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Сокращатель URL</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+   <h1 class="title">Сокращатель URL</h1>
+<?php
+  if(isset($_SESSION['feedback'])) {
+   echo "<p>".$_SESSION['feedback']."</p>";
+   unset($_SESSION['feedback']);
+  }
+?>
+   <form action="shorten.php" method="post">
+    <input type="url" name="url" placeholder="Введите URL" autocomplete="off">
+    <input type="submit" value="Сократить">
+   </form>
+  </div>
+</body>
+</html>
---
+++ shortener.php
@@ -0,0 +1,43 @@
+<?php
+  class Shortener {
+   protected $db;
+
+   public function __construct() {
+    $this->db = new Mysqli('localhost', 'yellow', '31101993', 'shortener');
+   }
+
+   public function generateCode($num) {
+    return base_convert($num, 10, 36);
+   }
+
+   public function makeCode($url) {
+
+    $url = $this->db->escape_string($url);
+
+    $exsists = $this->db->query("SELECT short_url FROM url_short WHERE long_url = '{$url}'");
+
+    if($exsists->num_rows) {
+     return $exsists->fetch_object()->code;
+    } else {
+     $this->db->query("INSERT INTO url_short(long_url) VALUE('{$url}')");
+
+     $code = $this->generateCode($this->db->insert_id);
+
+     $this->db->query("UPDATE url_short SET short_url = '{$code}' WHERE long_url = '{$url}'");
+
+     return $code;
+    }
+   }
+
+   public function getUrl($code) {
+    $code = $this->db->escape_string($code);
+    $code = $this->db->query("SELECT long_url FROM url_short WHERE short_url = '$code'");
+
+    if($code->num_rows) {
+     return $code->fetch_object()->url;
+    }
+
+    return '';
+   }
+  }
+?>
