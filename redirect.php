de035164a18e5240c22b05d1a6b8541cf7f01763
Yelaman Rakhimberlin <yelaman.rakhimberlin@gmail.com>
2021-08-24 02:41:52 +0600

    updated shorten and redirect

--- redirect.php
+++ redirect.php
@@ -1,17 +1,22 @@
 <?php
 //Этот файл должен проводить перевод с сокращенной ссылки на длинную ссылку в базе данных, но не работает, возможно проблема в одинарных кавычках,
 //но я попробовал и не работает, есть серьезный вопрос нужен ли в конце header'а адрес url?
+session_start();
   require_once "shortener.php";
   require_once "classes/shortener.php";
+  require_once "shorten.php";
 
   if(isset($_GET['code'])) {
+    print_r("fine!");
    $s = new Shortener();
    $code = $_GET['code'];
    if($url = $s->getUrl($code)) {
-    header('Location: {$url}');
+    header("Location: {$url}");
     exit();
    }
+  } else {
+    echo "fuck you";
   }
-
-  header('Location: {$index.php}');
+  echo "fuck you2";
+  header("Location: {index.php}");
 ?>
--- shorten.php
+++ shorten.php
@@ -11,7 +11,8 @@
    $url = $_POST['url'];
 
    if($code = $s->makeCode($url)) {
-    $_SESSION['feedback'] = "Готово! Вот ваша ссылка: <a href='http://localhost:3333/$code'>http://localhost:3333/$code</a>";
+    $_SESSION['feedback'] = "Готово! Вот ваша ссылка: <a href='http://localhost:3333/?code=$code'>http://localhost:3333/?code=$code</a>";
+    
    } else {
     $_SESSION['feedback'] = "Ошибка! Возможно, некорректный URL?";
     if(!$_SESSION) {
